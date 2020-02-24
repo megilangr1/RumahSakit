6c462b983a607c77406794628b8c2b280fa09444
Nazoru002 <nanazero123@gmail.com>
2020-02-24 10:52:39 +0700

    update

--- app/Http/Controllers/Admin/DoctorsController.php
+++ app/Http/Controllers/Admin/DoctorsController.php
@@ -110,7 +110,6 @@ class DoctorsController extends Controller
 
             return view('admin.doctors.edit', compact('doctor', 'edit', 'service'));
         } catch (\Exception $e) {
-            dd($e);
             session()->flash('error', 'Terjadi Kesalahan');
             return redirect()->back();
         }
